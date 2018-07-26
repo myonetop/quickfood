6ceeb3c14a9ef18b28f742bed0279ca63c0d2c6a
myone <34979727+myonetop@users.noreply.github.com>
2018-07-26 23:44:36 +0900

    Add files via upload

---
+++ app/components/Image/messages.js
@@ -0,0 +1,14 @@
+/*
+ * Image Messages
+ *
+ * This contains all the text for the Image component.
+ */
+
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  header: {
+    id: 'app.components.Image.header',
+    defaultMessage: 'This is the Image component !',
+  },
+});
